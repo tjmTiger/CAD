34caf747672ca2ba95254d3939e93cc4b45eaf0e
tjm_tiger <tomekmazurek1605@gmail.com>
2025-08-02 20:17:43 +0200

    add placeholders for constraints

---
+++ constraint.py
@@ -0,0 +1,32 @@
+class straight():
+    pass
+
+class curve():
+    pass
+
+class parallel():
+    pass
+
+class perpendicular():
+    pass
+
+class coincident():
+    pass
+
+class tangent():
+    pass
+
+class concentric():
+    pass
+
+class equal_radius():
+    pass
+
+class distance():
+    pass
+
+class angle():
+    pass
+
+class radius():
+    pass
