e81f93311a03cd54bdfbd6ef86fc4a00da029185
tjm_tiger <tomekmazurek1605@gmail.com>
2025-08-06 18:06:00 +0200

    Update ecs.py

--- ecs.py
+++ ecs.py
@@ -1,15 +1,6 @@
 from typing import TypeAlias
 # https://austinmorlan.com/posts/entity_component_system/
 
-################
-## Components ##
-################
-
-class Component():
-    def __init__(self):
-        # ex. position, rotation etc.
-        pass
-
 """
 horisontal
 vertical
@@ -26,36 +17,19 @@ distance_between
 angle_between
 """
 
-##############
-## Entities ##
-##############
-Entity: TypeAlias = int # Entity is just an id number
+################
+## Components ##
+################
 
-##############
-
-class EntityManager():
+class Line():
     def __init__(self):
-        self._MAX_ENTITIES = 5000
-        self._available_entities = list(range(self._MAX_ENTITIES-1, -1, -1)) # stack of items
-        self._entities = list()
-        self._signatures = dict()
-    
-    def create_entity(self):
-        entity = self._available_entities.pop()
-        self._entities.append(entity)
-        return entity
-    
-    def destroy_entity(self, entity: Entity):
-        self._entities.remove(entity)
-        self._signatures.pop(entity)
-        self._available_entities.append(entity)
-    
-    def set_signature(self, entity: Entity, signature): # a signature tracks what components an entity has
-        self._signatures[entity] = signature
-    
-    def get_signature(self, entity: Entity):
-        return self._signatures[entity]
+        # ex. position, rotation etc.
+        pass
 
-class ComponentArray():
-    def __init__(self):
+################
+## Properties ##
+################
+
+class Point():
+    def __init__():
         pass
